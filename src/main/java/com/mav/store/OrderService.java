3f7498e4532ec86fd8440769f83e284641ebfd64
Mav <riherr00@gmail.com>
2026-01-11 16:39:47 +0300

    SI on both classes

--- src/main/java/com/mav/store/OrderService.java
+++ src/main/java/com/mav/store/OrderService.java
@@ -3,9 +3,11 @@ package com.mav.store;
 public class OrderService {
     private PaymentService paymentService;
 
-    public OrderService(PaymentService paymentService) {
-        this.paymentService = paymentService;
-    }
+    public OrderService(){}
+
+//    public OrderService(PaymentService paymentService) {
+//        this.paymentService = paymentService;
+//    }
 
     public void placeOrder() {
         paymentService.processPayment(10);
