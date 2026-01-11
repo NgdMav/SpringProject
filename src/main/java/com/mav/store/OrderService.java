d9ff8fff63a37485901a2e244f580ee73f0dc0c9
Mav <riherr00@gmail.com>
2026-01-11 16:53:31 +0300

    Service and Autowired annot added to get auto imjected dependances with IoC

--- src/main/java/com/mav/store/OrderService.java
+++ src/main/java/com/mav/store/OrderService.java
@@ -1,13 +1,18 @@
 package com.mav.store;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
     private PaymentService paymentService;
 
     public OrderService(){}
 
-//    public OrderService(PaymentService paymentService) {
-//        this.paymentService = paymentService;
-//    }
+    @Autowired
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
 
     public void placeOrder() {
         paymentService.processPayment(10);
