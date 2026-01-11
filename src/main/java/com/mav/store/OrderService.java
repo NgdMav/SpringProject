aaae5eebc7bd0b941f30263621d7d810d80e2dc0
Mav <riherr00@gmail.com>
2026-01-11 16:22:13 +0300

    DI in order service

--- src/main/java/com/mav/store/OrderService.java
+++ src/main/java/com/mav/store/OrderService.java
@@ -1,8 +1,14 @@
 package com.mav.store;
 
 public class OrderService {
+
+    private PaymentService paymentService;
+
+    public OrderService(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
     public void placeOrder() {
-        var paymentService = new StripePaymentService();
         paymentService.processPayment(10);
     }
 }
